6d55b17d42398d077bbe83202af3f1450b1f3a52
Eric Rodrigues <er601432@gmail.com>
2021-11-27 14:30:14 -0300

    chore: organizing structure the code

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { UsersModule } from './modules/users/users.module';
       database: process.env.TYPEORM_DATABASE,
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
-      entities: [Users, Marketplace, Address,Products, Lists],
+      entities: [Users, Marketplace, Address, Products, Lists],
       retryDelay: 3000,
       retryAttempts: 10,
       synchronize: true,
@@ -30,7 +30,7 @@ import { UsersModule } from './modules/users/users.module';
     UsersModule,
     MarketplaceModule,
     ProductsModule,
-    ListsModule
+    ListsModule,
   ],
   controllers: [],
   providers: [],
