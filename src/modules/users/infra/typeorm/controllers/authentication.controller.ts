7688e1758ea4ed737085fe996d4eaea3aaea7c37
Eric Rodrigues <er601432@gmail.com>
2021-11-27 20:01:55 -0300

    feat: adjust authenticate methods

--- src/modules/users/infra/typeorm/controllers/authentication.controller.ts
+++ src/modules/users/infra/typeorm/controllers/authentication.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Post,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthenticateUserService } from '../../../services/authenticate-user.service';
-
-interface IAuth {
-  email: string;
-  password: string;
-}
+import { IAuth } from '../../../dtos/IAuthDTO';
 
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private authenticateUserService: AuthenticateUserService) {}
 
+  @UsePipes(ValidationPipe)
+  @UseInterceptors(ClassSerializerInterceptor)
   @Post()
   async createSession(@Body() { email, password }: IAuth): Promise<any> {
     return this.authenticateUserService.execute({ email, password });
--- src/shared/ensureAuthenticatedMiddleware.ts
+++ src/shared/ensureAuthenticatedMiddleware.ts
@@ -1,4 +1,8 @@
-import { NotFoundException } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import auth from '../config/auth';
@@ -17,15 +21,15 @@ export default function ensureAuthenticatedMiddleware(
   res: Response,
   next: NextFunction,
 ): void {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    throw new NotFoundException('JWT is missing');
-  }
-
-  const [, token] = authHeader.split(' ');
-
   try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new NotFoundException('JWT is missing');
+    }
+
+    const [, token] = authHeader.split(' ');
+
     const decoded = verify(token, auth.secret);
     const { id, email, name } = decoded as TokenPayload;
 
@@ -37,6 +41,6 @@ export default function ensureAuthenticatedMiddleware(
 
     return next();
   } catch (err) {
-    throw err;
+    throw new UnauthorizedException('Token JWT inválido');
   }
 }
