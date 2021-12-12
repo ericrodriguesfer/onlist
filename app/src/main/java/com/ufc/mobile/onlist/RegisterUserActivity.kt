69887ab4dbb9ffeba56eff7e1bdb8a6d89469090
Eric Rodrigues <er601432@gmail.com>
2021-12-12 13:32:10 -0300

    feat: create screen for register users

---
+++ app/src/main/java/com/ufc/mobile/onlist/RegisterUserActivity.kt
@@ -0,0 +1,11 @@
+package com.ufc.mobile.onlist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RegisterUserActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+    }
+}
