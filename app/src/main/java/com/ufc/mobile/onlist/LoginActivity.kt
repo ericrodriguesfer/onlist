d2e4537ce7311508caf74419a65f846bf9baa04e
Eric Rodrigues <er601432@gmail.com>
2021-12-18 11:29:21 -0300

    feat: break apart custom toast in dedicated class

--- app/src/main/java/com/ufc/mobile/onlist/LoginActivity.kt
+++ app/src/main/java/com/ufc/mobile/onlist/LoginActivity.kt
@@ -8,53 +8,16 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import com.ufc.mobile.onlist.util.ToastCustom
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.buttonActionLoginForm).setOnClickListener { showToast(SUCCESS, "Deu certo caraio, funfou") }
-    }
-
-    private fun showToast(type: Int, message: String) {
-        val view = findViewById<ViewGroup>(R.id.container_toast)
-        val v = layoutInflater.inflate(R.layout.custom_toast, view)
-
-        val imgMessage: ImageView = v.findViewById<ImageView>(R.id.img_toast)
-        val textMessage: TextView = v.findViewById<TextView>(R.id.txt_message)
-
-        when (type) {
-            SUCCESS -> {
-                v.background = ContextCompat.getDrawable(this, R.drawable.toast_success)
-                imgMessage.setImageDrawable(ContextCompat.getDrawable(this,
-                    R.drawable.icon_check_toast))
-                textMessage.setTextColor(resources.getColor(R.color.color_success_toast))
-            }
-            WARNING -> {
-                v.background = ContextCompat.getDrawable(this, R.drawable.toast_warning)
-                imgMessage.setImageDrawable(ContextCompat.getDrawable(this,
-                    R.drawable.icon_warning_toast))
-                textMessage.setTextColor(resources.getColor(R.color.color_warning_toast))
-            }
-            ERROR -> {
-                v.background = ContextCompat.getDrawable(this, R.drawable.toast_error)
-                imgMessage.setImageDrawable(ContextCompat.getDrawable(this,
-                    R.drawable.icon_error_toast))
-                textMessage.setTextColor(resources.getColor(R.color.color_error_toast))
-            }
+        findViewById<View>(R.id.buttonActionLoginForm).setOnClickListener {
+            val toastCustom = ToastCustom(ToastCustom.SUCCESS, "O Toast funcionou caraiu", this)
+            toastCustom.getToast().show()
         }
-
-        textMessage.text = message
-        val toast = Toast(this)
-        toast.view = v
-        toast.duration = Toast.LENGTH_SHORT
-        toast.show()
-    }
-
-    companion object {
-        public const val SUCCESS = 1
-        public const val WARNING = 2
-        public const val ERROR = 3
     }
 }
--- app/src/main/java/com/ufc/mobile/onlist/constants/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.ufc.mobile.onlist.constants
-
-class Constants {
- companion object {
-     public const val SUCCESS = 1
-     public const val WARNING = 2
-     public const val ERROR = 3
- }
-}
