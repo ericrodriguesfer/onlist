ad14e3c8eb5b6a5e3eaffd10931f6256cdd93152
Eric Rodrigues <er601432@gmail.com>
2022-01-10 09:20:53 -0300

    fix: adjust id in elements for remove duplicateds ids

--- app/src/main/java/com/ufc/mobile/onlist/LoginActivity.kt
+++ app/src/main/java/com/ufc/mobile/onlist/LoginActivity.kt
@@ -2,12 +2,7 @@ package com.ufc.mobile.onlist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import com.ufc.mobile.onlist.util.ToastCustom
 
 class LoginActivity : AppCompatActivity() {
@@ -15,7 +10,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.buttonActionLoginForm).setOnClickListener {
+        findViewById<View>(R.id.loginAction).setOnClickListener {
             val toastCustom = ToastCustom(ToastCustom.SUCCESS, "O Toast funcionou caraiu", this)
             toastCustom.getToast().show()
         }
